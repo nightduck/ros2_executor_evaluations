d93b2411fefea5d0a9027a7646f561eb6aa3f989
Oren Bell <oren.bell@wustl.edu>
2024-05-15 14:49:11 +0000

    Changes to automation
    Signed-off-by: Oren Bell <oren.bell@wustl.edu>

--- timers_only_benchmark.sh
+++ timers_only_benchmark.sh
@@ -1,42 +1,44 @@
 #!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+   echo "Script needs root, please run: "
+   echo "sudo env PATH=\"\$PATH\" LD_LIBRARY_PATH=\"\$LD_LIBRARY_PATH\" !! "
+   exit 1
+fi
 
 duration=5
 
+rm -r ~/.ros/tracing/timers-only-*
 source install/setup.bash
 
 # TODO: ros2 trace start/stop don't actually work and result in empty traces
 
-rm -r ~/.ros/tracing/timers-only-rm-ro
 ros2 trace start timers-only-rm-ro
-sudo env PATH="$PATH" LD_LIBRARY_PATH="$LD_LIBRARY_PATH" ./install/lib/rtss_evaluation/timers_only $duration rm ro | tee timers-only-rm-ro.log
+./install/lib/rtss_evaluation/timers_only $duration rm ro | tee timers-only-rm-ro.log
 ros2 trace stop timers-only-rm-ro
 
-rm -r ~/.ros/tracing/timers-only-rm-re
 ros2 trace start timers-only-rm-re
-sudo env PATH="$PATH" LD_LIBRARY_PATH="$LD_LIBRARY_PATH" ./install/lib/rtss_evaluation/timers_only $duration rm re | tee timers-only-rm-re.log
+./install/lib/rtss_evaluation/timers_only $duration rm re | tee timers-only-rm-re.log
 ros2 trace stop timers-only-rm-re
 
-rm -r ~/.ros/tracing/timers-only-edf-ro
 ros2 trace start timers-only-edf-ro
-sudo env PATH="$PATH" LD_LIBRARY_PATH="$LD_LIBRARY_PATH" ./install/lib/rtss_evaluation/timers_only $duration edf ro | tee timers-only-edf-ro.log
+./install/lib/rtss_evaluation/timers_only $duration edf ro | tee timers-only-edf-ro.log
 ros2 trace stop timers-only-edf-ro
 
-rm -r ~/.ros/tracing/timers-only-edf-re
 ros2 trace start timers-only-edf-re
-sudo env PATH="$PATH" LD_LIBRARY_PATH="$LD_LIBRARY_PATH" ./install/lib/rtss_evaluation/timers_only $duration edf re | tee timers-only-edf-re.log
+./install/lib/rtss_evaluation/timers_only $duration edf re | tee timers-only-edf-re.log
 ros2 trace stop timers-only-edf-re
 
-rm -r ~/.ros/tracing/timers-only-events-ro
 ros2 trace start timers-only-events-ro
-sudo env PATH="$PATH" LD_LIBRARY_PATH="$LD_LIBRARY_PATH" ./install/lib/rtss_evaluation/timers_only $duration events ro | tee timers-only-events-ro.log
+./install/lib/rtss_evaluation/timers_only $duration events ro | tee timers-only-events-ro.log
 ros2 trace stop timers-only-events-ro
 
-rm -r ~/.ros/tracing/timers-only-events-re
 ros2 trace start timers-only-events-re
-sudo env PATH="$PATH" LD_LIBRARY_PATH="$LD_LIBRARY_PATH" ./install/lib/rtss_evaluation/timers_only $duration events re | tee timers-only-events-re.log
+./install/lib/rtss_evaluation/timers_only $duration events re | tee timers-only-events-re.log
 ros2 trace stop timers-only-events-re
 
-rm -r ~/.ros/tracing/timers-only-default
 ros2 trace start timers-only-default
-sudo env PATH="$PATH" LD_LIBRARY_PATH="$LD_LIBRARY_PATH" ./install/lib/rtss_evaluation/timers_only $duration default | tee timers-only-default.log
+./install/lib/rtss_evaluation/timers_only $duration default | tee timers-only-default.log
 ros2 trace stop timers-only-default
+
+cp -r ~/.ros/tracing/timers-only-* .
+chown -R $SUDO_USER:$SUDO_USER timers-only-*
