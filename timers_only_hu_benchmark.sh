a0fa6bb084771748e5cf65937dfa96938c5f2027
Oren Bell <oren.bell@wustl.edu>
2024-05-16 04:45:37 +0000

    Added 2nd script for high utilization timers-only
    Signed-off-by: Oren Bell <oren.bell@wustl.edu>

--- timers_only_hu_benchmark.sh
+++ timers_only_hu_benchmark.sh
@@ -14,31 +14,31 @@ source install/setup.bash
 # TODO: ros2 trace start/stop don't actually work and result in empty traces
 
 ros2 trace start timers-only-rm-ro
-./install/lib/rtss_evaluation/timers_only $duration rm ro | tee timers-only-rm-ro.log
+./install/lib/rtss_evaluation/timers_only_high_utilization $duration rm ro | tee timers-only-rm-ro.log
 ros2 trace stop timers-only-rm-ro
 
 ros2 trace start timers-only-rm-re
-./install/lib/rtss_evaluation/timers_only $duration rm re | tee timers-only-rm-re.log
+./install/lib/rtss_evaluation/timers_only_high_utilization $duration rm re | tee timers-only-rm-re.log
 ros2 trace stop timers-only-rm-re
 
 ros2 trace start timers-only-edf-ro
-./install/lib/rtss_evaluation/timers_only $duration edf ro | tee timers-only-edf-ro.log
+./install/lib/rtss_evaluation/timers_only_high_utilization $duration edf ro | tee timers-only-edf-ro.log
 ros2 trace stop timers-only-edf-ro
 
 ros2 trace start timers-only-edf-re
-./install/lib/rtss_evaluation/timers_only $duration edf re | tee timers-only-edf-re.log
+./install/lib/rtss_evaluation/timers_only_high_utilization $duration edf re | tee timers-only-edf-re.log
 ros2 trace stop timers-only-edf-re
 
 ros2 trace start timers-only-events-ro
-./install/lib/rtss_evaluation/timers_only $duration events ro | tee timers-only-events-ro.log
+./install/lib/rtss_evaluation/timers_only_high_utilization $duration events ro | tee timers-only-events-ro.log
 ros2 trace stop timers-only-events-ro
 
 ros2 trace start timers-only-events-re
-./install/lib/rtss_evaluation/timers_only $duration events re | tee timers-only-events-re.log
+./install/lib/rtss_evaluation/timers_only_high_utilization $duration events re | tee timers-only-events-re.log
 ros2 trace stop timers-only-events-re
 
 ros2 trace start timers-only-default
-./install/lib/rtss_evaluation/timers_only $duration default | tee timers-only-default.log
+./install/lib/rtss_evaluation/timers_only_high_utilization $duration default | tee timers-only-default.log
 ros2 trace stop timers-only-default
 
 cp -r ~/.ros/tracing/timers-only-* .
