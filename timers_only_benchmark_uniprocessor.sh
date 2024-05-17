9ed9c280e57c2971e0156f96c554b92e6c3dfd79
Oren Bell <oren.bell@wustl.edu>
2024-05-17 22:57:08 +0000

    Removed kernel traces to save on processing time
    Signed-off-by: Oren Bell <oren.bell@wustl.edu>

--- timers_only_benchmark_uniprocessor.sh
+++ timers_only_benchmark_uniprocessor.sh
@@ -12,87 +12,87 @@ rm -r ~/.ros/tracing/trace-timers-only/*
 source /opt/ros/rolling/setup.bash
 source install/setup.bash
 
-ros2 trace start -k sched_switch -l trace-timers-only.rm.ro.uu
+ros2 trace start trace-timers-only.rm.ro.uu
 taskset 0x8 ./install/lib/rtss_evaluation/timers_only $duration rm ro | tee trace-timers-only.rm.ro.uu.log
 ros2 trace stop trace-timers-only.rm.ro.uu
 
-ros2 trace start -k sched_switch -l trace-timers-only.rm.re.uu
+ros2 trace start trace-timers-only.rm.re.uu
 taskset 0x8 ./install/lib/rtss_evaluation/timers_only $duration rm re | tee trace-timers-only.rm.re.uu.log
 ros2 trace stop trace-timers-only.rm.re.uu
 
-ros2 trace start -k sched_switch -l trace-timers-only.edf.ro.uu
+ros2 trace start trace-timers-only.edf.ro.uu
 taskset 0x8 ./install/lib/rtss_evaluation/timers_only $duration edf ro | tee trace-timers-only.edf.ro.uu.log
 ros2 trace stop trace-timers-only.edf.ro.uu
 
-ros2 trace start -k sched_switch -l trace-timers-only.edf.re.uu
+ros2 trace start trace-timers-only.edf.re.uu
 taskset 0x8 ./install/lib/rtss_evaluation/timers_only $duration edf re | tee trace-timers-only.edf.re.uu.log
 ros2 trace stop trace-timers-only.edf.re.uu
 
-ros2 trace start -k sched_switch -l trace-timers-only.events.ro.uu
+ros2 trace start trace-timers-only.events.ro.uu
 taskset 0x8 ./install/lib/rtss_evaluation/timers_only $duration events ro | tee trace-timers-only.events.ro.uu.log
 ros2 trace stop trace-timers-only.events.ro.uu
 
-ros2 trace start -k sched_switch -l trace-timers-only.events.re.uu
+ros2 trace start trace-timers-only.events.re.uu
 taskset 0x8 ./install/lib/rtss_evaluation/timers_only $duration events re | tee trace-timers-only.events.re.uu.log
 ros2 trace stop trace-timers-only.events.re.uu
 
-ros2 trace start -k sched_switch -l trace-timers-only.default.uu
+ros2 trace start trace-timers-only.default.uu
 taskset 0x8 ./install/lib/rtss_evaluation/timers_only $duration default | tee trace-timers-only.default.uu.log
 ros2 trace stop trace-timers-only.default.uu
 
-ros2 trace start -k sched_switch -l trace-timers-only.rm.ro.hu
+ros2 trace start trace-timers-only.rm.ro.hu
 taskset 0x8 ./install/lib/rtss_evaluation/timers_only_high_utilization $duration rm ro | tee trace-timers-only.rm.ro.hu.log
 ros2 trace stop trace-timers-only.rm.ro.hu
 
-ros2 trace start -k sched_switch -l trace-timers-only.rm.re.hu
+ros2 trace start trace-timers-only.rm.re.hu
 taskset 0x8 ./install/lib/rtss_evaluation/timers_only_high_utilization $duration rm re | tee trace-timers-only.rm.re.hu.log
 ros2 trace stop trace-timers-only.rm.re.hu
 
-ros2 trace start -k sched_switch -l trace-timers-only.edf.ro.hu
+ros2 trace start trace-timers-only.edf.ro.hu
 taskset 0x8 ./install/lib/rtss_evaluation/timers_only_high_utilization $duration edf ro | tee trace-timers-only.edf.ro.hu.log
 ros2 trace stop trace-timers-only.edf.ro.hu
 
-ros2 trace start -k sched_switch -l trace-timers-only.edf.re.hu
+ros2 trace start trace-timers-only.edf.re.hu
 taskset 0x8 ./install/lib/rtss_evaluation/timers_only_high_utilization $duration edf re | tee trace-timers-only.edf.re.hu.log
 ros2 trace stop trace-timers-only.edf.re.hu
 
-ros2 trace start -k sched_switch -l trace-timers-only.events.ro.hu
+ros2 trace start trace-timers-only.events.ro.hu
 taskset 0x8 ./install/lib/rtss_evaluation/timers_only_high_utilization $duration events ro | tee trace-timers-only.events.ro.hu.log
 ros2 trace stop trace-timers-only.events.ro.hu
 
-ros2 trace start -k sched_switch -l trace-timers-only.events.re.hu
+ros2 trace start trace-timers-only.events.re.hu
 taskset 0x8 ./install/lib/rtss_evaluation/timers_only_high_utilization $duration events re | tee trace-timers-only.events.re.hu.log
 ros2 trace stop trace-timers-only.events.re.hu
 
-ros2 trace start -k sched_switch -l trace-timers-only.default.hu
+ros2 trace start trace-timers-only.default.hu
 taskset 0x8 ./install/lib/rtss_evaluation/timers_only_high_utilization $duration default | tee trace-timers-only.default.hu.log
 ros2 trace stop trace-timers-only.default.hu
 
-ros2 trace start -k sched_switch -l trace-timers-only.rm.ro.ou
+ros2 trace start trace-timers-only.rm.ro.ou
 taskset 0x8 ./install/lib/rtss_evaluation/timers_only_over_utilization $duration rm ro | tee trace-timers-only.rm.ro.ou.log
 ros2 trace stop trace-timers-only.rm.ro.ou
 
-ros2 trace start -k sched_switch -l trace-timers-only.rm.re.ou
+ros2 trace start trace-timers-only.rm.re.ou
 taskset 0x8 ./install/lib/rtss_evaluation/timers_only_over_utilization $duration rm re | tee trace-timers-only.rm.re.ou.log
 ros2 trace stop trace-timers-only.rm.re.ou
 
-ros2 trace start -k sched_switch -l trace-timers-only.edf.ro.ou
+ros2 trace start trace-timers-only.edf.ro.ou
 taskset 0x8 ./install/lib/rtss_evaluation/timers_only_over_utilization $duration edf ro | tee trace-timers-only.edf.ro.ou.log
 ros2 trace stop trace-timers-only.edf.ro.ou
 
-ros2 trace start -k sched_switch -l trace-timers-only.edf.re.ou
+ros2 trace start trace-timers-only.edf.re.ou
 taskset 0x8 ./install/lib/rtss_evaluation/timers_only_over_utilization $duration edf re | tee trace-timers-only.edf.re.ou.log
 ros2 trace stop trace-timers-only.edf.re.ou
 
-ros2 trace start -k sched_switch -l trace-timers-only.events.ro.ou
+ros2 trace start trace-timers-only.events.ro.ou
 taskset 0x8 ./install/lib/rtss_evaluation/timers_only_over_utilization $duration events ro | tee trace-timers-only.events.ro.ou.log
 ros2 trace stop trace-timers-only.events.ro.ou
 
-ros2 trace start -k sched_switch -l trace-timers-only.events.re.ou
+ros2 trace start trace-timers-only.events.re.ou
 taskset 0x8 ./install/lib/rtss_evaluation/timers_only_over_utilization $duration events re | tee trace-timers-only.events.re.ou.log
 ros2 trace stop trace-timers-only.events.re.ou
 
-ros2 trace start -k sched_switch -l trace-timers-only.default.ou
+ros2 trace start trace-timers-only.default.ou
 taskset 0x8 ./install/lib/rtss_evaluation/timers_only_over_utilization $duration default | tee trace-timers-only.default.ou.log
 ros2 trace stop trace-timers-only.default.ou
 
