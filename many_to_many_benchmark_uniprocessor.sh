97f68cc08a91fb1323aa13e8f7b809916ba3e496
Oren Bell <virtualoren@gmail.com>
2024-10-22 11:43:40 +0200

    Saved response time logs to data file in many-to-many benchmark
    Signed-off-by: Oren Bell <virtualoren@gmail.com>

--- many_to_many_benchmark_uniprocessor.sh
+++ many_to_many_benchmark_uniprocessor.sh
@@ -15,54 +15,78 @@ OU=90
 rm -r ~/.ros/tracing/trace-many-to-many.*
 source install/setup.bash
 
-ros2 trace start trace-many-to-many.edf.uu
+ros2 trace start trace-many-to-many.edf.$UU
 # chrt -f 50 taskset 0x8 ./install/lib/paper_evaluation/many_to_many_pub $duration edf | tee trace-many-to-many.edf.uu.log
-taskset 0x8 ./install/lib/paper_evaluation/many_to_many_pub $duration edf | tee trace-many-to-many.edf.uu.log
-ros2 trace stop trace-many-to-many.edf.uu
+taskset 0x8 ./install/lib/paper_evaluation/many_to_many_pub $duration edf | tee trace-many-to-many.edf.$UU.log
+ros2 trace stop trace-many-to-many.edf.$UU
+mkdir -p data/response_time.edf.$UU/
+mv *.node.txt data/response_time.edf.$UU/
 
-ros2 trace start trace-many-to-many.rm.uu
-taskset 0x8 ./install/lib/paper_evaluation/many_to_many_pub $duration rm | tee trace-many-to-many.rm.uu.log
-ros2 trace stop trace-many-to-many.rm.uu
+ros2 trace start trace-many-to-many.rm.$UU
+taskset 0x8 ./install/lib/paper_evaluation/many_to_many_pub $duration rm | tee trace-many-to-many.rm.$UU.log
+ros2 trace stop trace-many-to-many.rm.$UU
+mkdir -p data/response_time.rm.$UU/
+mv *.node.txt data/response_time.rm.$UU/
 
-ros2 trace start trace-many-to-many.events.uu
-taskset 0x8 ./install/lib/paper_evaluation/many_to_many_pub $duration events | tee trace-many-to-many.events.uu.log
-ros2 trace stop trace-many-to-many.events.uu
+ros2 trace start trace-many-to-many.events.$UU
+taskset 0x8 ./install/lib/paper_evaluation/many_to_many_pub $duration events | tee trace-many-to-many.events.$UU.log
+ros2 trace stop trace-many-to-many.events.$UU
+mkdir -p data/response_time.events.$UU/
+mv *.node.txt data/response_time.events.$UU/
 
-ros2 trace start trace-many-to-many.default.uu
-taskset 0x8 ./install/lib/paper_evaluation/many_to_many_pub $duration default | tee trace-many-to-many.default.uu.log
-ros2 trace stop trace-many-to-many.default.uu
+ros2 trace start trace-many-to-many.default.$UU
+taskset 0x8 ./install/lib/paper_evaluation/many_to_many_pub $duration default | tee trace-many-to-many.default.$UU.log
+ros2 trace stop trace-many-to-many.default.$UU
+mkdir -p data/response_time.default.$UU/
+mv *.node.txt data/response_time.default.$UU/
 
-ros2 trace start trace-many-to-many.edf.hu
-taskset 0x8 ./install/lib/paper_evaluation/many_to_many_pub $duration edf | tee trace-many-to-many.edf.hu.log
-ros2 trace stop trace-many-to-many.edf.hu
+ros2 trace start trace-many-to-many.edf.$HU
+taskset 0x8 ./install/lib/paper_evaluation/many_to_many_pub $duration edf | tee trace-many-to-many.edf.$HU.log
+ros2 trace stop trace-many-to-many.edf.$HU
+mkdir -p data/response_time.edf.$HU/
+mv *.node.txt data/response_time.edf.$HU/
 
-ros2 trace start trace-many-to-many.rm.hu
-taskset 0x8 ./install/lib/paper_evaluation/many_to_many_pub $duration rm | tee trace-many-to-many.rm.hu.log
-ros2 trace stop trace-many-to-many.rm.hu
+ros2 trace start trace-many-to-many.rm.$HU
+taskset 0x8 ./install/lib/paper_evaluation/many_to_many_pub $duration rm | tee trace-many-to-many.rm.$HU.log
+ros2 trace stop trace-many-to-many.rm.$HU
+mkdir -p data/response_time.rm.$HU/
+mv *.node.txt data/response_time.rm.$HU/
 
-ros2 trace start trace-many-to-many.events.hu
-taskset 0x8 ./install/lib/paper_evaluation/many_to_many_pub $duration events | tee trace-many-to-many.events.hu.log
-ros2 trace stop trace-many-to-many.events.hu
+ros2 trace start trace-many-to-many.events.$HU
+taskset 0x8 ./install/lib/paper_evaluation/many_to_many_pub $duration events | tee trace-many-to-many.events.$HU.log
+ros2 trace stop trace-many-to-many.events.$HU
+mkdir -p data/response_time.events.$HU/
+mv *.node.txt data/response_time.events.$HU/
 
-ros2 trace start trace-many-to-many.default.hu
-taskset 0x8 ./install/lib/paper_evaluation/many_to_many_pub $duration default | tee trace-many-to-many.default.hu.log
-ros2 trace stop trace-many-to-many.default.hu
+ros2 trace start trace-many-to-many.default.$HU
+taskset 0x8 ./install/lib/paper_evaluation/many_to_many_pub $duration default | tee trace-many-to-many.default.$HU.log
+ros2 trace stop trace-many-to-many.default.$HU
+mkdir -p data/response_time.default.$HU/
+mv *.node.txt data/response_time.default.$HU/
 
-ros2 trace start trace-many-to-many.edf.ou
-taskset 0x8 ./install/lib/paper_evaluation/many_to_many_pub $duration edf | tee trace-many-to-many.edf.ou.log
-ros2 trace stop trace-many-to-many.edf.ou
+ros2 trace start trace-many-to-many.edf.$OU
+taskset 0x8 ./install/lib/paper_evaluation/many_to_many_pub $duration edf | tee trace-many-to-many.edf.$OU.log
+ros2 trace stop trace-many-to-many.edf.$OU
+mkdir -p data/response_time.edf.$OU/
+mv *.node.txt data/response_time.edf.$OU/
 
-ros2 trace start trace-many-to-many.rm.ou
-taskset 0x8 ./install/lib/paper_evaluation/many_to_many_pub $duration rm | tee trace-many-to-many.rm.ou.log
-ros2 trace stop trace-many-to-many.rm.ou
+ros2 trace start trace-many-to-many.rm.$OU
+taskset 0x8 ./install/lib/paper_evaluation/many_to_many_pub $duration rm | tee trace-many-to-many.rm.$OU.log
+ros2 trace stop trace-many-to-many.rm.$OU
+mkdir -p data/response_time.edf.$OU/
+mv *.node.txt data/response_time.edf.$OU/
 
-ros2 trace start trace-many-to-many.events.ou
-taskset 0x8 ./install/lib/paper_evaluation/many_to_many_pub $duration events | tee trace-many-to-many.events.ou.log
-ros2 trace stop trace-many-to-many.events.ou
+ros2 trace start trace-many-to-many.events.$OU
+taskset 0x8 ./install/lib/paper_evaluation/many_to_many_pub $duration events | tee trace-many-to-many.events.$OU.log
+ros2 trace stop trace-many-to-many.events.$OU
+mkdir -p data/response_time.edf.$OU/
+mv *.node.txt data/response_time.edf.$OU/
 
-ros2 trace start trace-many-to-many.default.ou
-taskset 0x8 ./install/lib/paper_evaluation/many_to_many_pub $duration default | tee trace-many-to-many.default.ou.log
-ros2 trace stop trace-many-to-many.default.ou
+ros2 trace start trace-many-to-many.default.$OU
+taskset 0x8 ./install/lib/paper_evaluation/many_to_many_pub $duration default | tee trace-many-to-many.default.$OU.log
+ros2 trace stop trace-many-to-many.default.$OU
+mkdir -p data/response_time.edf.$OU/
+mv *.node.txt data/response_time.edf.$OU/
 
 cp -r ~/.ros/tracing/trace-many-to-many.* ./data/
 mv trace-many-to-many* ./data/
